5405e4160b208926612007fbac1e26f095a6aae4
Omar <oa624488@gmail.com>
2025-03-24 17:36:26 -0600

    first commit

---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { get } from 'env-var';
+
+
+export const envs = {
+
+  PORT: get('PORT').required().asPortNumber(),
+  
+  POSTGRES_URL: get('POSTGRES_URL').default('public').asString(),
+
+  JWT_SEED: get('JWT_SEED').required().asString(),
+
+}
+
+
+
